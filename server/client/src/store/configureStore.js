29b43d5ada215fb1b2754e19b8c42484b1934010
Brian Fajardo <bmf5@hotmail.com>
2017-08-22 00:56:48 -0400

    Heroku postbuild production script.

--- server/client/src/store/configureStore.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-import reduxThunk from 'redux-thunk'
-
-import rootReducer from '../reducers'
-
-const middleware = applyMiddleware(reduxThunk)
-const configureStore = initialState => createStore(rootReducer, initialState, middleware)
-
-export default configureStore
